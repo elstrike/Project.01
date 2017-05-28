52e2a24f0d1a521eedd9b7225d24941037b709be
Seongji <elstrike@naver.com>
2017-05-28 22:49:24 +0900

    Initial Commit

---
+++ Project.01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project.01
+//
+//  Created by Seong-ji Cho on 2017. 5. 28..
+//  Copyright © 2017년 Seong-ji Cho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
